997c46910125014469eb1e89f0282e8051417076
Russell Coight <rj.coight@gmail.com>
2018-08-01 17:11:49 +1000

    animation working

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 
 let preloader_info = Object.create(null);
 preloader_info["dom_elem"] = document.querySelector(".Preloader__");
-preloader_info["dom_elem_anim_frames"] = document.querySelectorAll("[class*=Preloader__AnimationFrame]");
+preloader_info["dom_elem_progress_bar"] = document.querySelector(".Preloader__ProgressBar");
+preloader_info["dom_elem_anim_frames"] = document.querySelectorAll(".Preloader__AnimationFrame__");
 preloader_info["num_anim_frames"] = preloader_info.dom_elem_anim_frames.length;
 preloader_info["is_active"] = false;
-preloader_info["anim_duration"] = 1300;
-preloader_info["time_per_frame"] = preloader_info.num_anim_frames / preloader_info.anim_duration;
+preloader_info["anim_duration"] = 500;
+preloader_info["time_per_frame"] = preloader_info.anim_duration / preloader_info.num_anim_frames;
 preloader_info["when_last_anim_update"] = 0;
 preloader_info["since_last_anim_update"] = 0;
 preloader_info["active_frame_num"] = 0;
@@ -47,8 +48,8 @@ function preload_page(num_resources_to_load) {
   const NUM_RESOURCES_LOADED = window.performance.getEntriesByType("resource").length;
   const RESOURCES_LOADED_STATUS_STR = `${(NUM_RESOURCES_LOADED / num_resources_to_load) * 100}%`;
   
-  preloader.width = RESOURCES_LOADED_STATUS_STR;
-  preloader.dataset.status = RESOURCES_LOADED_STATUS_STR;
+  preloader_info.dom_elem_progress_bar.width = RESOURCES_LOADED_STATUS_STR;
+  preloader_info.dom_elem_progress_bar.dataset.status = RESOURCES_LOADED_STATUS_STR;
   
   if (NUM_RESOURCES_LOADED != num_resources_to_load) {
     setTimeout(preload_page, 0, num_resources_to_load);
