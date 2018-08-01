daf8436a9aa865215b1c7b593009399ec746e819
Russell Coight <rj.coight@gmail.com>
2018-08-01 15:42:14 +1000

    further

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 ---
 ---
-function anim_preloader(start_time, preloader, preloader_anim_frames) {
-  preloader_anim_frames[anim_preloader.cur_frame].style.display = "table-cell"; 
+
+let preloader_info = Object.create(null);
+preloader_info["dom_elem"] = document.querySelector(".Preloader__");
+preloader_info["dom_elem_anim_frames"] = document.querySelectorAll("[class*=Preloader__AnimationFrame]");
+preloader_info["num_anim_frames"] = preloader_info.dom_elem_anim_frames.length;
+preloader_info["is_active"] = false;
+preloader_info["anim_duration"] = 1300;
+preloader_info["time_per_frame"] = preloader_info.num_anim_frames / preloader_info.anim_duration;
+preloader_info["when_last_anim_update"] = 0;
+preloader_info["since_last_anim_update"] = 0;
+preloader_info["active_frame_num"] = 0;
+
+function anim_preloader(cur_time) {
+  if (preloader_info.is_active) {
+    if (preloader_info.when_last_anim_update === 0) {
+      preloader_info.when_last_anim_update = cur_time; 
+    }
+    
+    preloader_info.since_last_anim_update = cur_time - preloader_info.when_last_anim_update;
+
+    if (preloader_info.since_last_anim_update > preloader_info.time_per_frame) {
+       preloader_info.dom_elem_anim_frames[preloader_info.active_frame_num].style.display = "none"; 
+
+       const NEXT_FRAME_INDEX = (preloader_info.active_frame_num + 1) % preloader_info.num_anim_frames; 
+       preloader_info.dom_elem_anim_frames[NEXT_FRAME_INDEX].style.display = "table-cell"; 
+
+       preloader_info.when_last_anim_update = cur_time;
+
+       preloader_info.active_frame_num = NEXT_FRAME_INDEX; 
+    }
+
+    window.requestAnimationFrame(anim_preloader);
+
+  } else {
+    return; 
+  }
 }
 
 function preload_page(num_resources_to_load) {
-  window.requestAnimationFrame((start_time) => {
-    anim_preloader(start_time, preloader, preloader_anim_frames);
-  });
+  if (!preloader_info.is_active) {
+    window.requestAnimationFrame(anim_preloader);
+    preloader_info.is_active = true;
+  }
 
   const NUM_RESOURCES_LOADED = window.performance.getEntriesByType("resource").length;
   const RESOURCES_LOADED_STATUS_STR = `${(NUM_RESOURCES_LOADED / num_resources_to_load) * 100}%`;
@@ -16,23 +51,12 @@ function preload_page(num_resources_to_load) {
   preloader.dataset.status = RESOURCES_LOADED_STATUS_STR;
   
   if (NUM_RESOURCES_LOADED != num_resources_to_load) {
-  	setTimeout(preload_page, 0, num_resources_to_load);
+    setTimeout(preload_page, 0, num_resources_to_load);
   } else {
-        preloader.style["animation-play-state"] = "start"; 
-        page.style["animation-play-state"] = "start"; 
-  	return;
+    preloader_info.is_active = false;
+    preloader_info.dom_elem.style["animation-play-state"] = "start"; 
+
+    document.querySelector(".Page__").style["animation-play-state"] = "start"; 
   }
 }
 
-function {{ site.abbrev }}(e) {
-  const ELEMS = Object.freeze({
-    "PRELOADER": document.querySelector(".Preloader__"),
-    "PRELOADER_ANIM_FRAMES": document.querySelectorAll("[class*=Preloader__AnimationFrame]"),
-    "PAGE": document.querySelector(".Page__")
-  });
-
-  preload_page({{ page.num_resources | plus: 4 }}, ELEMS.PRELOADER, ELEMS.PRELOADER_ANIM_FRAMES, ELEMS.PAGE);
-
-} 
-
-document.addEventListener("DOMContentLoaded", {{ site.abbrev }});
