1ce87327818b1b7b1a9e364a727c054989adb229
Ryan <re.mcclue@gmail.com>
2018-08-21 18:27:36 +1000

    As

--- _includes/js/preload-page.js
+++ _includes/js/preload-page.js
@@ -1,3 +1,6 @@
+---
+---
+
 async function preload_page(website_name, page_name, num_resources_to_load) {
   const RESOURCES_HAVE_LOADED = () => { 
     return window.performance.getEntriesByType("resource").length >= 
@@ -6,17 +9,23 @@ async function preload_page(website_name, page_name, num_resources_to_load) {
 
   const UPDATE = async () => {
 	if (RESOURCES_HAVE_LOADED()) {
-	  preloader_image_dom_elem.style.animationPlayState = "paused";
-	  preloader_dom_elem.style.opacity = 0;
+	  anim_frame_img_dom_elem.style.animationPlayState = "paused";
+	  dom_elem.style.opacity = 0;
 	  await _sleep(500); // wait until opacity animation finished
 		
-	  document.body.removeChild(PRELOADER_CSS);
-	  document.body.removeChild(PRELOADER_HTML);
+	  document.body.removeChild(PRELOADER_CSS_DOM_ELEM);
+	  document.body.removeChild(PRELOADER_HTML_DOM_ELEM);
       document.documentElement.style = `
         width: 100%;
 	    height: 100%;
 	  `;
 	} else {
+	  const SCALE_AMOUNT = window.performance.getEntriesByType("resource").length /
+	                         num_resources_to_load;
+      
+      progress_bar_dom_elem.style.transform = `scaleX(${SCALE_AMOUNT})`;
+      progress_bar_dom_elem.innerHTML = `${parseInt(SCALE_AMOUNT * 100, 10)}%`;
+
       setTimeout(UPDATE, 100);		
 	}
   }
@@ -27,7 +36,9 @@ async function preload_page(website_name, page_name, num_resources_to_load) {
 	    <h1 class="Preloader__WebsiteName"> 
 		  ${website_name} 
 		</h1>
-        <section class="Preloader__Image" /></section>
+        <section class="Preloader__AnimFrame__"/>
+		  <div class="Preloader__AnimFrame__Img"></div>
+		</section>
 		<section class="Preloader__ProgressBar__">
           <div class="Preloader__ProgressBar__Bar"></div>
 		</section>
@@ -40,29 +51,69 @@ async function preload_page(website_name, page_name, num_resources_to_load) {
     const PRELOADER_CSS = `
       <style>
 	    .Preloader__ {
+		  width: 100%;
+		  height: 100%;
+		  display: flex;
+		  flex-flow: column wrap;
+		  align-items: center;
+		  justify-content: center;
 		  opacity: 1;
 		  transition: opacity 500ms ease-in-out;
+		  background-color: {{ site.primary_color }}
+	      color: white;		
 		} 
-		.Preloader__Image {
-		  width:
-		  height: 
-	      background-image: url();
+
+		.Preloader__AnimFrame__ {
+		  width: 100%;
+		  height: auto;
+		}
+
+		.Preloader__AnimFrame__Img {
+		  width: 100%:
+		  height: auto;
+	      background: url("{{ site.url }}/images/preloader.svg") 
+		                no-repeat 0% 0%;
+		  background-size: 100%;
 		  animation: PreloaderImageAnim 2s steps(8) infinite;
 		}
 
         @keyframes PreloaderImageAnim {
-		  0% {
-		    transform: translateX(0);	  
+		  from {
+		    background-position: 0% 0%;  
 		  }
-		  100% {
-		    transform: translateX(100%);  
+		  to {
+		    background-position: 0% 100%;  
 		  }
 		}
+
+		.Preloader__ProgressBar__ {
+		  position: relative;
+          width: 10em;
+		  height: 2em;
+		  border: 0.5em solid {{ site.primary_color }};
+		}
+
+		.Preloader__ProgressBar__Bar {
+		  position: absolute;
+          top: 0.2em; left: 0.2em; right: 0.2em; bottom: 0.2em;
+		  width: 100%;
+		  height: 100%;
+		  transform: scaleX(0.05);
+		  transition: transform 250ms ease-in-out;
+		  background-color: {{ site.primary_color }};
+		}
+
       </style>
 	`;
 
+    const HTML_DOM_ELEM = _str_to_dom_elem(PRELOADER_HTML);
+    const CSS_DOM_ELEM = _str_to_dom_elem(PRELOADER_CSS);
 
-    document.appendChild(_str_to_dom_elem(PRELOADER_HTML));
+    document.appendChild(HTML_DOM_ELEM);
+    document.appendChild(CSS_DOM_ELEM);
+
+    const ANIM_FRAME_IMG_DOM_ELEM = document.querySelector(".Preloader__AnimFrame__Img");
+    const PROGRESS_BAR_DOM_ELEM = document.querySelector(".Preloader__ProgressBar__Bar");
 
     document.documentElement.style = `
       width: 100vw;
@@ -71,12 +122,4 @@ async function preload_page(website_name, page_name, num_resources_to_load) {
 
     setTimeout(UPDATE, 100, PROGRESS_BAR_DOM_ELEM, );
   }
-
-
-	const SCALE_AMOUNT = NUM_RESOURCES_LOADED / num_resources_to_load; 
-    const RESOURCES_LOADED_STATUS_STR = `${parseInt(SCALE_AMOUNT * 100, 10)}%`;
-    
-    preloader.progress_bar_bar_dom_elem.style.transform = `scale(${SCALE_AMOUNT}, 1)`;
-    preloader.progress_bar_bar_dom_elem.innerHTML = RESOURCES_LOADED_STATUS_STR;
 }
-
